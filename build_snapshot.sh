63979b3c79e8c3e7322294004d00a4d97c50d0e3
Ubuntu User <ubuntu@gitbuilder-deb-amd64.(none)>
2011-02-10 13:07:33 -0800

    build_all.sh: take dists on cmdline

--- build_snapshot.sh
+++ build_snapshot.sh
@@ -3,6 +3,7 @@
 bindir=`dirname $0`
 releasedir=$1
 pbuilddir=$2
+dists=$3
 
 [ -z "$releasedir" ] && exit 1
 [ -z "$pbuilddir" ] && exit 1
@@ -16,7 +17,8 @@ trap cleanup SIGINT EXIT
 $bindir/release_tarball.sh $releasedir $versionfile dosuffix
 vers=`cat $versionfile`
 
-$bindir/build_dsc.sh $releasedir $vers
+$bindir/build_dsc.sh $releasedir $vers 1 $dists
+
 sudo $bindir/build_debs.sh $releasedir $pbuilddir $vers
 $bindir/sign_debs.sh $releasedir $vers
 
