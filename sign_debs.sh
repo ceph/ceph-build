b6c5187f592ea92db0a003eef20618b8567bcc3b
Sage Weil <sage@newdream.net>
2011-03-22 12:46:29 -0700

    sign_debs: sign specific file types; usage

--- sign_debs.sh
+++ sign_debs.sh
@@ -2,16 +2,27 @@
 
 set -e
 
+usage() {
+    echo "usage: $0 releasedir vers key [filetypetosign]"
+}
+
 releasedir=$1
 cephvers=$2
 keyid=$3
+what=$4
 
-[ -z "$releasedir" ] && echo specify releasedir && exit 1
-[ -z "$cephvers" ] && echo specify version && exit 1
-[ ! -d "$releasedir/$cephvers" ] && echo missing $releasedir/$cephvers && exit 1
+[ -z "$what" ] && what="dsc changes"
+
+[ -z "$releasedir" ] && echo specify releasedir && usage && exit 1
+[ -z "$cephvers" ] && echo specify version && usage && exit 1
+[ ! -d "$releasedir/$cephvers" ] && echo missing $releasedir/$cephvers && usage && exit 1
 
 [ -z "$keyid" ] && echo specify keyid && exit 1
 
 echo version $cephvers
 
-yes | debsign -k$keyid $releasedir/$cephvers/*.{changes,dsc}
+for w in $what
+do
+    yes | debsign -k$keyid $releasedir/$cephvers/*.$w
+done
+
