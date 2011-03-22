26d6509e96bf605a29f11f38e24ab3168db94890
Sage Weil <sage@newdream.net>
2011-03-22 12:47:39 -0700

    build_snapshot: usage, sign dsc before build
    (fewer build warnings)

--- build_snapshot.sh
+++ build_snapshot.sh
@@ -3,14 +3,19 @@
 set -e
 
 bindir=`dirname $0`
+
+usage() {
+    echo "usage: $0 releasedir pbuilddir [dists...]"
+}
+
 releasedir=$1
 pbuilddir=$2
 shift
 shift
 dists="$*"
 
-[ -z "$releasedir" ] && exit 1
-[ -z "$pbuilddir" ] && exit 1
+[ -z "$releasedir" ] && usage && exit 1
+[ -z "$pbuilddir" ] && usage && exit 1
 
 versionfile=`mktemp`
 cleanup() {
@@ -25,13 +30,12 @@ $bindir/release_tarball.sh $releasedir $versionfile
 vers=`cat $versionfile`
 
 $bindir/build_dsc.sh $releasedir $vers 1 $dists
-
+$bindir/sign_debs.sh $releasedir $vers 03C3951A dsc
 sudo $bindir/build_debs.sh $releasedir $pbuilddir $vers
-$bindir/sign_debs.sh $releasedir $vers 03C3951A
+$bindir/sign_debs.sh $releasedir $vers 03C3951A changes
 
 # probably a better way, but
 rm $versionfile
-
 trap true INT EXIT
 
 exit 0
