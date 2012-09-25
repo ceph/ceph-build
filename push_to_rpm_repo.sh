990dbbf1ad87aa3e47c63ae34236ce307716ab30
Gary Lowell <glowell@inktank.com>
2012-09-24 17:54:32 -0700

    ceph-build:  new script and updates to push rpms to repo.

---
+++ push_to_rpm_repo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+releasedir=$1
+repo=$2
+cephvers=$3
+
+keyid=3CF7ABC8
+
+usage() {
+    echo "usage: $0 releasedir repodir version component"
+}
+
+[ -z "$releasedir" ] && echo specify releasedir && usage && exit 1
+[ -z "$repo" ] && echo specify reprepro dir && usage && exit 1
+[ -z "$cephvers" ] && echo specify version && usage && exit 1
+[ ! -d "$releasedir/$cephvers" ] && echo missing $releasedir/$cephvers && usage && exit 1
+
+bindir=`dirname $0`
+
+echo version $cephvers
+#echo component $component
+
+mkdir -p $repo
+
+# For each distribution that we've built
+for dist in `ls -1 $releasedir/$cephvers/rpm`
+do
+    echo dist $dist
+    # build and sign yum index in release directory
+    $bindir/gen_yum_repo.sh $releasedir/$cephvers/rpm/$dist $keyid
+    # stage the results
+    for dir in $releasedir/$cephvers/rpm/$dist/RPMS/*
+    do
+        mkdir -p $repo/$cephvers/
+        cp -a $dir  $repo/$cephvers/.
+    done
+done
+
+echo done
