9809d2e1840d250c8d63d457f107be19ac5561c4
Gary Lowell <gary.lowell@inktank.com>
2013-05-07 13:23:53 -0700

    ceph-build:  Autosign RPM packages
    Add expect script to supply null passphrase when signing rpms.
    
    Signed-off-by: Gary Lowell  <gary.lowell@inktank.com>

--- sign_rpms.sh
+++ sign_rpms.sh
@@ -3,15 +3,13 @@
 set -e
 
 usage() {
-    echo "usage: $0 releasedir vers key [filetypetosign]"
+    echo "usage: $0 releasedir vers key"
 }
 
 releasedir=$1
 cephvers=$2
 keyid=$3
-what=$4
-
-[ -z "$what" ] && what="dsc changes"
+bindir=`dirname $0`
 
 [ -z "$releasedir" ] && echo specify releasedir && usage && exit 1
 [ -z "$cephvers" ] && echo specify version && usage && exit 1
@@ -19,10 +17,15 @@ what=$4
 
 [ -z "$keyid" ] && echo specify keyid && exit 1
 
-echo version $cephvers key $keyid
+echo "signing rpms, version $cephvers key $keyid"
 
 # Sign all the RPMs for this release
-rpm_list=`find $releasedir/$cephvers/rpm -name "*.rpm" -print`
-rpm --addsign --define "_gpg_name $keyid" $rpm_list
+#rpm --addsign --define "_gpg_name $keyid" $rpm
+#use expect wrapper to supply null passphrase
+
+for rpm in `find ${releasedir}${cephvers}/rpm -name "*.rpm"`
+do
+    $bindir/rpm-autosign.exp --define "_gpg_name $keyid" $rpm
+done
 
 echo done
