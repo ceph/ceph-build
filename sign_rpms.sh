8a22c8eff91baf9ad4a0b9e4e189108bc10929a3
Gary Lowell <gary.lowell@inktank.com>
2013-06-06 17:36:04 -0700

    sign_rpm.sh:  Fix bug
    There isn't an rpm sub directory in the repo.
    
    Signed-off-by: Gary Lowell  <gary.lowell@inktank.com>

--- sign_rpms.sh
+++ sign_rpms.sh
@@ -3,17 +3,17 @@
 set -e
 
 usage() {
-    echo "usage: $0 releasedir vers key"
+    echo "usage: $0 repodir vers key"
 }
 
-releasedir=$1
+repodir=$1
 cephvers=$2
 keyid=$3
 bindir=`dirname $0`
 
-[ -z "$releasedir" ] && echo specify releasedir && usage && exit 1
+[ -z "$repodir" ] && echo specify repodir && usage && exit 1
 [ -z "$cephvers" ] && echo specify version && usage && exit 1
-[ ! -d "$releasedir/$cephvers" ] && echo missing $releasedir/$cephvers && usage && exit 1
+[ ! -d "$repodir/$cephvers" ] && echo missing $repodir/$cephvers && usage && exit 1
 
 [ -z "$keyid" ] && echo specify keyid && exit 1
 
@@ -23,7 +23,7 @@ echo "signing rpms, version $cephvers key $keyid"
 #rpm --addsign --define "_gpg_name $keyid" $rpm
 #use expect wrapper to supply null passphrase
 
-for rpm in `find ${releasedir}${cephvers}/rpm -name "*.rpm"`
+for rpm in `find ${repodir}/${cephvers} -name "*.rpm"`
 do
     $bindir/rpm-autosign.exp --define "_gpg_name $keyid" $rpm
 done
