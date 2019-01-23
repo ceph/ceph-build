2ce0068fbece0ecb433620943f60cfeed236ffbb
Guillaume Abrioux <gabrioux@redhat.com>
2019-01-23 16:45:05 +0100

    build_utils: change workdir for tox (ceph-ansible)
    This commit reverts the previous commit and simply change the working
    directory when running tox, so the shebangs are much more shorter (<127
    char).
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- scripts/build_utils.sh
+++ scripts/build_utils.sh
@@ -751,7 +751,7 @@ function build_job_name() {
 }
 
 # shellcheck disable=SC2116
-if ! eval "$(echo "${TOX_RUN_ENV[@]}")" "$VENV"/tox -rv -e="$(build_job_name $RELEASE $DISTRIBUTION $DEPLOYMENT $SCENARIO)" -- --provider=libvirt; then echo "ERROR: Job didn't complete successfully or got stuck for more than 3h."
+if ! eval "$(echo "${TOX_RUN_ENV[@]}")" "$VENV"/tox --workdir="$TEMPVENV" -v -e="$(build_job_name $RELEASE $DISTRIBUTION $DEPLOYMENT $SCENARIO)" -- --provider=libvirt; then echo "ERROR: Job didn't complete successfully or got stuck for more than 3h."
   exit 1
 fi
 }
