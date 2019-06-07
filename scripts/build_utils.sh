9a9b4163b7763f11b02a584dfc261cad154d4741
Kefu Chai <kchai@redhat.com>
2019-06-07 23:51:47 +0800

    scripts/build_utils.sh: update ubuntu-toolchain-r mirrors
    the removed two do not have GCC-9, but the newly added mirros do.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- scripts/build_utils.sh
+++ scripts/build_utils.sh
@@ -552,9 +552,9 @@ echo "deb [lang=none] http://security.ubuntu.com/ubuntu $DIST-security main" >> 
   /etc/apt/sources.list.d/ubuntu-toolchain-r.list
 echo "deb [lang=none] http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu $DIST main" >> \
   /etc/apt/sources.list.d/ubuntu-toolchain-r.list
-echo "deb [arch=amd64 lang=none] http://mirror.cs.uchicago.edu/ubuntu-toolchain-r $DIST main" >> \
+echo "deb [arch=amd64 lang=none] http://mirror.nullivex.com/ppa/ubuntu-toolchain-r-test $DIST main >> \
   /etc/apt/sources.list.d/ubuntu-toolchain-r.list
-echo "deb [arch=amd64,i386 lang=none] http://mirror.yandex.ru/mirrors/launchpad/ubuntu-toolchain-r $DIST main" >> \
+echo "deb [arch=amd64 lang=none] http://deb.rug.nl/ppa/mirror/ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu $DIST main" >> \
   /etc/apt/sources.list.d/ubuntu-toolchain-r.list
 EOF
     else
