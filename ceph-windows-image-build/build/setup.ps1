94293330b7eafee85901e0b5668f73251bd88c81
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2022-04-29 13:08:43 +0300

    [ceph-windows-image-build] Fix Windows image build
    The `${VIRTIO_WIN_PATH}\guest-agent\qemu-ga-x86_64.msi` unattended
    installation fails with latest virtio.
    
    Refactor the setup scripts to only install the required drivers (`NetKVM`,
    `viostor`, `vioserial`), and use a single script (`first-logon.ps1`) for
    provisioning the Windows OS.
    
    Also, add a separate script `utils.ps1` with common PowerShell functions.

--- ceph-windows-image-build/build/setup.ps1
+++ ceph-windows-image-build/build/setup.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
+. "${PSScriptRoot}\utils.ps1"
+
 $VS_2019_BUILD_TOOLS_URL = "https://aka.ms/vs/16/release/vs_buildtools.exe"
 $WDK_URL = "https://download.microsoft.com/download/7/d/6/7d602355-8ae9-414c-ae36-109ece2aade6/wdk/wdksetup.exe"  # Windows 11 WDK (22000.1). It can be used to develop drivers for previous OS releases.
 $PYTHON3_URL = "https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe"
@@ -15,126 +17,6 @@ function Get-WindowsBuildInfo {
     $table.Columns.AddRange(@("Release", "Version", "Build"))
     $table.Rows.Add($p.ProductName, $p.ReleaseId, "$($p.CurrentBuild).$($p.UBR)") | Out-Null
     return $table
-}
-
-function Invoke-CommandLine {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String]$Command,
-        [String]$Arguments,
-        [Int[]]$AllowedExitCodes=@(0)
-    )
-    & $Command $Arguments.Split(" ")
-    if($LASTEXITCODE -notin $AllowedExitCodes) {
-        Throw "$Command $Arguments returned a non zero exit code ${LASTEXITCODE}."
-    }
-}
-
-function Start-ExecuteWithRetry {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [ScriptBlock]$ScriptBlock,
-        [Int]$MaxRetryCount=10,
-        [Int]$RetryInterval=3,
-        [String]$RetryMessage,
-        [Array]$ArgumentList=@()
-    )
-    $currentErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = "Continue"
-    $retryCount = 0
-    while ($true) {
-        try {
-            $res = Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
-            $ErrorActionPreference = $currentErrorActionPreference
-            return $res
-        } catch [System.Exception] {
-            $retryCount++
-            if ($retryCount -gt $MaxRetryCount) {
-                $ErrorActionPreference = $currentErrorActionPreference
-                Throw $_
-            } else {
-                $prefixMsg = "Retry(${retryCount}/${MaxRetryCount})"
-                if($RetryMessage) {
-                    Write-Host "${prefixMsg} - $RetryMessage"
-                } elseif($_) {
-                    Write-Host "${prefixMsg} - $($_.ToString())"
-                }
-                Start-Sleep $RetryInterval
-            }
-        }
-    }
-}
-
-function Start-FileDownload {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String]$URL,
-        [Parameter(Mandatory=$true)]
-        [String]$Destination,
-        [Int]$RetryCount=10
-    )
-    Write-Output "Downloading $URL to $Destination"
-    Start-ExecuteWithRetry `
-        -ScriptBlock { Invoke-CommandLine -Command "curl.exe" -Arguments "-L -s -o $Destination $URL" } `
-        -MaxRetryCount $RetryCount `
-        -RetryMessage "Failed to download '${URL}'. Retrying"
-    Write-Output "Successfully downloaded."
-}
-
-function Add-ToPathEnvVar {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String[]]$Path,
-        [Parameter(Mandatory=$false)]
-        [ValidateSet([System.EnvironmentVariableTarget]::User, [System.EnvironmentVariableTarget]::Machine)]
-        [System.EnvironmentVariableTarget]$Target=[System.EnvironmentVariableTarget]::Machine
-    )
-    $pathEnvVar = [Environment]::GetEnvironmentVariable("PATH", $Target).Split(';')
-    $currentSessionPath = $env:PATH.Split(';')
-    foreach($p in $Path) {
-        if($p -notin $pathEnvVar) {
-            $pathEnvVar += $p
-        }
-        if($p -notin $currentSessionPath) {
-            $currentSessionPath += $p
-        }
-    }
-    $env:PATH = $currentSessionPath -join ';'
-    $newPathEnvVar = $pathEnvVar -join ';'
-    [Environment]::SetEnvironmentVariable("PATH", $newPathEnvVar, $Target)
-}
-
-function Install-Tool {
-    [CmdletBinding(DefaultParameterSetName = "URL")]
-    Param(
-        [Parameter(Mandatory=$true, ParameterSetName = "URL")]
-        [String]$URL,
-        [Parameter(Mandatory=$true, ParameterSetName = "LocalPath")]
-        [String]$LocalPath,
-        [Parameter(ParameterSetName = "URL")]
-        [Parameter(ParameterSetName = "LocalPath")]
-        [String[]]$Params=@(),
-        [Parameter(ParameterSetName = "URL")]
-        [Parameter(ParameterSetName = "LocalPath")]
-        [Int[]]$AllowedExitCodes=@(0)
-    )
-    PROCESS {
-        $installerPath = $LocalPath
-        if($PSCmdlet.ParameterSetName -eq "URL") {
-            $installerPath = Join-Path $env:TEMP $URL.Split('/')[-1]
-            Start-FileDownload -URL $URL -Destination $installerPath
-        }
-        Write-Output "Installing ${installerPath}"
-        $p = Start-Process -FilePath $installerPath -ArgumentList $Params -NoNewWindow -PassThru -Wait
-        if($p.ExitCode -notin $AllowedExitCodes) {
-            Throw "Installation failed. Exit code: $($p.ExitCode)"
-        }
-        if($PSCmdlet.ParameterSetName -eq "URL") {
-            Start-ExecuteWithRetry `
-                -ScriptBlock { Remove-Item -Force -Path $installerPath -ErrorAction Stop } `
-                -RetryMessage "Failed to remove ${installerPath}. Retrying"
-        }
-    }
 }
 
 function Get-GitHubReleaseAssets {
