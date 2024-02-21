8b80a0113de2649ebfdb9a80543e605166b78b31
Redouane Kachach <rkachach@redhat.com>
2024-02-21 10:29:48 +0100

    ceph-build: fixing dependencies script
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- scripts/rook-orch/install-rook-e2e-deps.sh
+++ scripts/rook-orch/install-rook-e2e-deps.sh
@@ -16,25 +16,25 @@ install_docker(){
     sudo groupadd docker || true
     sudo usermod -aG docker $(id -un)
     sudo systemctl unmask docker
-    sudo systemctl start docker
+    sudo systemctl restart docker
     sudo chgrp "$(id -un)" /var/run/docker.sock
+
+    # wait for docker
+    sleep 10
 
     docker info
     docker container prune -f
 }
 
-# delete any existing minikube setup
-minikube delete
-
-# delete any existing libvirt socket
-sudo rm -rf /var/run/libvirt/libvirt-sock
-
 # install dependencies
 sudo apt update -y
-sudo apt install --reinstall -y qemu-kvm libvirt-daemon-driver-qemu libvirt-clients libvirt-daemon-system  runc python3
+sudo apt install --reinstall -y qemu-kvm libvirt-daemon-driver-qemu libvirt-clients libvirt-daemon-system libvirt-daemon runc python3
 sudo apt install --reinstall -y python3-pip
 install_docker
 
 # install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/v1.31.2/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# delete any existing minikube setup
+minikube delete
