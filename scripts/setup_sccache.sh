6004a859f184590792738264d84e443cf2407f91
Zack Cerza <zack@redhat.com>
2024-11-13 16:10:20 -0700

    Add ceph-dev-pipeline
    This may ultimately replace the existing ceph-*build jobs; for now, it is a
    work in-progress.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- scripts/setup_sccache.sh
+++ scripts/setup_sccache.sh
@@ -3,8 +3,10 @@
 
 set -ex
 
+SCCACHE_URL="https://github.com/mozilla/sccache/releases/download/v0.8.2/sccache-v0.8.2-$(uname -m)-unknown-linux-musl.tar.gz"
+
 function write_sccache_conf() {
-  export SCCACHE_CONF=$WORKSPACE/sccache.conf
+  export SCCACHE_CONF=${SCCACHE_CONF:-$WORKSPACE/sccache.conf}
   cat << EOF > $SCCACHE_CONF
 [cache.s3]
 bucket = "ceph-sccache"
@@ -28,6 +30,40 @@ EOF
 }
 
 function install_sccache () {
-  SCCACHE_URL="https://github.com/mozilla/sccache/releases/download/v0.8.2/sccache-v0.8.2-$(uname -m)-unknown-linux-musl.tar.gz"
-  curl -L $SCCACHE_URL | sudo tar --no-anchored --strip-components=1 -C /usr/local/bin/ -xzf - sccache
+  local sudo
+  if [ "$(id -u)" != "0" ]; then
+    sudo="sudo"
+  fi
+  curl -L $SCCACHE_URL | $sudo tar --no-anchored --strip-components=1 -C /usr/local/bin/ -xzf - sccache
+}
+
+function setup_pbuilderrc () {
+  cat >> ~/.pbuilderrc << EOF
+export SCCACHE="${SCCACHE}"
+export SCCACHE_CONF=/etc/sccache.conf
+export DWZ="${DWZ}"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+EOF
+  sudo cp ~/.pbuilderrc /root/.pbuilderrc
+}
+
+function setup_sccache_pbuilder_hook () {
+  for hook_dir in $(ls -d ~/.pbuilder/hook*.d); do
+    hook=$hook_dir/D09-setup-sccache
+    cp $BASH_SOURCE $hook
+    cat >> $hook << EOF
+if [ "$SCCACHE" = true ] ; then
+  write_sccache_conf
+  write_aws_credentials
+  install_sccache
+fi
+EOF
+    chmod +x $hook
+  done
+}
+
+function reset_sccache () {
+  sccache --zero-stats
+  sccache --stop-server
 }
