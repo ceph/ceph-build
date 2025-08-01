fbda4e49a05984d53353a458e5d2ff8689bead9d
Zack Cerza <zack@cerza.org>
2025-08-01 15:44:34 -0600

    Add scripts/setup_uv.sh
    Signed-off-by: Zack Cerza <zack@cerza.org>

---
+++ scripts/setup_uv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vim: ts=4 sw=4 expandtab
+
+function setup_pipx () {
+  command -v pipx || (
+    command -v apt && sudo apt install -y pipx
+    command -v dnf && sudo dnf install -y pipx
+  )
+  pipx ensurepath
+}
+
+function setup_uv () {
+  setup_pipx
+  pipx install uv
+}
+
+# If the script is executed (as opposed to sourced), run the function now
+if [ "$(basename -- "${0#-}")" = "$(basename -- "${BASH_SOURCE[0]}")" ]; then
+  setup_uv
+fi
