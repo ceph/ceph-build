9af84def9d05cdacba1610f07872ac8e5772adc2
Zack Cerza <zack@cerza.org>
2025-09-10 15:10:04 -0600

    setup_chacract.sh: Use setup_uv.sh
    Signed-off-by: Zack Cerza <zack@cerza.org>

--- scripts/setup_chacractl.sh
+++ scripts/setup_chacractl.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -ex
 # vim: ts=4 sw=4 expandtab
-command -v pipx || (
-  command -v apt && sudo apt install -y pipx
-  command -v dnf && sudo dnf install -y pipx
-)
-pipx ensurepath
-pipx install uv
-~/.local/bin/uv tool install chacractl
+"$WORKSPACE/scripts/setup_uv.sh"
+PATH=$PATH:$HOME/.local/bin
+uv tool install chacractl
 
 if [ -z "$chacra_url" ]; then
   chacra_url=$(curl -u "$SHAMAN_API_USER:$SHAMAN_API_KEY" https://shaman.ceph.com/api/nodes/next/)
 fi
-cat > $HOME/.chacractl << EOF
+cat > "$HOME/.chacractl" << EOF
 url = "$chacra_url"
 user = "$CHACRACTL_USER"
 key = "$CHACRACTL_KEY"
 ssl_verify = True
 EOF
-echo $chacra_url
+echo "$chacra_url"
