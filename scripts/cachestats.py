4fc9de2c0c4dc99bf842e5cef58b4751436d84d3
Zack Cerza <zack@cerza.org>
2025-10-03 16:06:07 -0600

    ceph-dev-pipeline: Record cache stats
    This is a work in progress

---
+++ scripts/cachestats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+from os import environ as env
+
+result = {
+    'url': env['BUILD_URL'],
+    'project': 'ceph',
+    'repo': env['CEPH_REPO'],
+    'branch': env['BRANCH'],
+    'sha1': env['SHA1'],
+    'distro': env['DIST'],
+    'arch': env['ARCH'],
+    'flavor': env['FLAVOR'],
+    'timestamp': int(env['BUILD_STARTED']),
+    'status': 'pass',
+}
+
+if 'BUILD_DURATION' in env:
+    result['duration'] = int(env['BUILD_DURATION'])
+
+if len(sys.argv) > 1:
+    raw_stats = json.loads(open(sys.argv[1]).read())
+    stats = {
+        'cache_requested': raw_stats['compile_requests'],
+        'cache_executed': raw_stats['requests_executed'],
+        'cache_hits': sum(raw_stats['cache_hits']['counts'].values()),
+        'cache_misses': sum(raw_stats['cache_misses']['counts'].values())
+    }
+    result.update(stats)
+
+print(json.dumps(result))
