00b5e37c4e55319193b871761ae26aaa1000687b
jitendrasahu1803 <jitendra.sahu1803@gmail.com>
2026-05-26 20:21:29 +0530

    Add support for 'libvirt=True'

--- builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
+++ builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
@@ -4,25 +4,24 @@ import sys
 inventory_file = sys.argv[1]
 node_input = sys.argv[2]
 
-# Always convert to FQDN
 node_name = node_input if "." in node_input else node_input + ".front.sepia.ceph.com"
 
 with open(inventory_file) as f:
     data = yaml.safe_load(f)
 
 labels_map = data.get("jenkins_labels", {})
-
-labels_str = labels_map.get(node_name)
-
-if not labels_str:
-    print("")
-    sys.exit(0)
+labels_str = labels_map.get(node_name, "")
 
 labels = labels_str.split()
 
+os_name = ""
+libvirt = "false"
+
 for label in labels:
     if label.startswith("installed-os-"):
-        print(label.replace("installed-os-", ""))
-        sys.exit(0)
+        os_name = label.replace("installed-os-", "")
+    if label == "libvirt":
+        libvirt = "true"
 
-print("")
+# Print both values in structured way
+print(f"{os_name},{libvirt}")
