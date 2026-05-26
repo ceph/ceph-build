7edadba09179ee8407fbd884886679005518d8f0
jitendrasahu1803 <jitendra.sahu1803@gmail.com>
2026-05-26 14:22:09 +0530

    amend

--- builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
+++ builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
@@ -1,18 +1,34 @@
 import yaml
 import sys
 
-file = sys.argv[1]
-node = sys.argv[2]
+inventory_file = sys.argv[1]
+node_input = sys.argv[2]
 
-with open(file) as f:
+# Normalize node name to FQDN if needed
+if "." not in node_input:
+    node_name = node_input + ".front.sepia.ceph.com"
+else:
+    node_name = node_input
+
+with open(inventory_file) as f:
     data = yaml.safe_load(f)
 
-labels = data.get("jenkins_builder_labels", {}).get(node, [])
+labels_map = data.get("jenkins_labels", {})
 
-for l in labels:
-    if l.startswith("installed-os-"):
-        print(l.replace("installed-os-", ""))
+labels_str = labels_map.get(node_name)
+
+if not labels_str:
+    print("ERROR: No labels found for node")
+    sys.exit(1)
+
+# Split space separated labels
+labels = labels_str.split()
+
+for label in labels:
+    if label.startswith("installed-os-"):
+        os_name = label.replace("installed-os-", "")
+        print(os_name)
         sys.exit(0)
 
-print("ERROR: No installed-os-* label found")
+print("ERROR: installed-os label not found")
 sys.exit(1)
