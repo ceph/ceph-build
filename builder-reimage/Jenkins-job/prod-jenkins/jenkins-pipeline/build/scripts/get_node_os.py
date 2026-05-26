08dee8d671f42f0a95786b8f59aac74a64e820fb
jitendrasahu1803 <jitendra.sahu1803@gmail.com>
2026-05-26 14:31:38 +0530

    amend

--- builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
+++ builder-reimage/Jenkins-job/prod-jenkins/jenkins-pipeline/build/scripts/get_node_os.py
@@ -4,11 +4,8 @@ import sys
 inventory_file = sys.argv[1]
 node_input = sys.argv[2]
 
-# Normalize node name to FQDN if needed
-if "." not in node_input:
-    node_name = node_input + ".front.sepia.ceph.com"
-else:
-    node_name = node_input
+# Always convert to FQDN
+node_name = node_input if "." in node_input else node_input + ".front.sepia.ceph.com"
 
 with open(inventory_file) as f:
     data = yaml.safe_load(f)
@@ -18,17 +15,14 @@ labels_map = data.get("jenkins_labels", {})
 labels_str = labels_map.get(node_name)
 
 if not labels_str:
-    print("ERROR: No labels found for node")
-    sys.exit(1)
+    print("")
+    sys.exit(0)
 
-# Split space separated labels
 labels = labels_str.split()
 
 for label in labels:
     if label.startswith("installed-os-"):
-        os_name = label.replace("installed-os-", "")
-        print(os_name)
+        print(label.replace("installed-os-", ""))
         sys.exit(0)
 
-print("ERROR: installed-os label not found")
-sys.exit(1)
+print("")
